de5c3bd52f94fd0646ff047a65fa0b933e19546c
Marcus Dashe <marcussdashe.developer@gmail.com>
2023-12-13 11:15:27 +0100

    update: added custom themes, fixed Carla card overflow issue and refactored local storage service

--- lib/util/local_storage/local_storage_service.dart
+++ lib/util/local_storage/local_storage_service.dart
@@ -25,6 +25,36 @@ static final LocalStorageService _instance = LocalStorageService._internal();
     await _prefs?.setString(key, value);
   }
 
+  // Save a value of any type T in shared preferences
+  Future<void> saveValue<T>(String key, T value) async {
+    if (value is String) {
+      await _prefs?.setString(key, value);
+    } else if (value is int) {
+      await _prefs?.setInt(key, value);
+    } else if (value is double) {
+      await _prefs?.setDouble(key, value);
+    } else if (value is bool) {
+      await _prefs?.setBool(key, value);
+    } else {
+      throw ArgumentError("Unsupported value type");
+    }
+  }
+
+
+  // Get a value of any type T from shared preferences
+  T? getValue<T>(String key) {
+    if (T == String) {
+      return _prefs?.getString(key) as T?;
+    } else if (T == int) {
+      return _prefs?.getInt(key) as T?;
+    } else if (T == double) {
+      return _prefs?.getDouble(key) as T?;
+    } else if (T == bool) {
+      return _prefs?.getBool(key) as T?;
+    } else {
+      throw ArgumentError("Unsupported value type");
+    }
+  }
   // Get a String value from shared preferences
   String? getString(String key) {
     return _prefs?.getString(key);
